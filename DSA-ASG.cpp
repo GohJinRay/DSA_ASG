cbf81825d02e5642eedf1703554cb88605412dc6
gohjinray <jinray2003@gmail.com>
2023-07-21 15:04:34 +0800

    edited it to test the functionalities of Admin class

--- DSA-ASG.cpp
+++ DSA-ASG.cpp
@@ -18,8 +18,8 @@ int main()
 {
 	QueuePtr newQueue;
 	//LinkedList_Customer llC; // added this list
-	Customer cust1(1, "a", "123", 123, false); // added the list into here
-	Customer cust2(2, "b", "124", 124, false); // and here
+	Customer cust1(1, "John", "123", 123, false); // added the list into here
+	Customer cust2(2, "Mary", "124", 124, false); // and here
 	Order* order;
 
 	order = cust1.createOrder(1);
@@ -35,16 +35,34 @@ int main()
 	order->addFoodItem(1);
 	newQueue.enqueue(*order);
 
-	cout << "Shows cust1's order" << endl;
-	cust1.getOrderList().OrderListprint();
-	cout << "Shows cust2's order" << endl;
-	cust2.getOrderList().OrderListprint();
+	//cout << "Shows cust1's order" << endl;
+	//cust1.getOrderList().OrderListprint();
+	//cout << "Shows cust2's order" << endl;
+	//cust2.getOrderList().OrderListprint();
 
-	cout << "Shows what's in the queue currently" << endl;
-	newQueue.displayItems();
-	newQueue.dequeue();
-	cout << "Hello" << endl;
-	newQueue.displayItems();
+	//cout << "Shows what's in the queue currently" << endl;
+	//newQueue.displayItems();
+	//newQueue.dequeue();
+	//cout << "Hello" << endl;
+	//newQueue.displayItems();
+
+
+	// testing dictionary
+	Dictionary CustomerDict;
+	CustomerDict.add(cust1.getName(),cust1);
+	CustomerDict.add(cust2.getName(), cust2);
+
+
+	// testing admin methods
+	Admin admin1(999, "admin", "password", 12345678, true);
+	//LinkedList_Customer orderL;
+	//orderL.addOrder(order);
+	//admin1.viewOrders(orderL);
+	//admin1.updateStatus(orderL);
+	//admin1.viewOrders(orderL);
+	//admin1.viewCustInfo(orderL, 3);
+
+
 	return 0;
 }
 

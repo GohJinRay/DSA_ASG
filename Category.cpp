831aed3eb85f863d38f2bf80563af2c25611ef8f
gohjinray <jinray2003@gmail.com>
2023-08-07 22:13:42 +0800

    added category class

---
+++ Category.cpp
@@ -0,0 +1,10 @@
+#include "Category.h"
+
+Category::Category() {};
+
+Category::Category(int catid, string catname, SortedArray catarray) {
+	catID = catid;
+	catName = catname;
+	catArray = catArray;
+}
+
